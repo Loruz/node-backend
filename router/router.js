a19c8228f40f7a45b0d3b6f09feedf712ad07300
Vytautas <vpreiks@gmail.com>
2018-07-17 14:05:47 +0300

    verify token middleware for all routes that are not auth

--- router/router.js
+++ router/router.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router({})
+const VerifyToken = require('./VerifyToken')
 
 router.use('/auth', require('../controllers/AuthController'))
+router.all('*', VerifyToken)
 
 module.exports = router
